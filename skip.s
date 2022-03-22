2f37c7baccdd0fccd999c95652b31cead3aa224e
Caaby <38215101+Caaby@users.noreply.github.com>
2022-03-22 21:42:36 +0800

    Create skip.s

---
+++ skip.s
@@ -0,0 +1,10 @@
+#!name=skip proxy list
+#!desc=跳过部分应用的代理检测
+#!system=ios
+#!url=
+
+[General]
+# 百度    - www.baidu.com
+# 建行生活 - yunbusiness.ccb.com
+# 苹果抓包 - captive.apple.com
+skip-proxy = %INSERT% www.baidu.com, yunbusiness.ccb.com, captive.apple.com
