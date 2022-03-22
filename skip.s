aaf087b8f3d0292d3635109c3e01e1ad5532d363
Caaby <38215101+Caaby@users.noreply.github.com>
2022-03-22 21:44:09 +0800

    Update skip.s

--- skip.s
+++ skip.s
@@ -1,10 +1,10 @@
 #!name=skip proxy list
 #!desc=跳过部分应用的代理检测
 #!system=ios
-#!url=
+#!url=https://raw.githubusercontent.com/caaby/w/surge/skip.s
 
 [General]
-# 百度    - www.baidu.com
-# 建行生活 - yunbusiness.ccb.com
-# 苹果抓包 - captive.apple.com
+# >> 百度搜索 - www.baidu.com
+# >> 建行生活 - yunbusiness.ccb.com
+# >> 苹果抓包 - captive.apple.com
 skip-proxy = %INSERT% www.baidu.com, yunbusiness.ccb.com, captive.apple.com
