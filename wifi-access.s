61aa64d20830a83130faaafa21a08bb689c4259b
Caaby <38215101+Caaby@users.noreply.github.com>
2022-03-25 20:01:24 +0800

    Create wifi-access.s

---
+++ wifi-access.s
@@ -0,0 +1,12 @@
+#!name=wifi access
+#!desc=允许wifi访问，可用于http和socks5代理
+#!url https://raw.githubusercontent.com/caaby/w/surge/wifi-access
+
+[General]
+# Wi-Fi 访问
+# >> 允许 Wi-Fi 访问（默认不启用）
+allow-wifi-access = false
+# >> HTTP 代理服务端口（默认为"6152"）
+wifi-access-http-port = 6152
+# >> SOCKS5 代理服务端口（默认为"6153"）
+wifi-access-socks5-port = 6153
