1983525e19e197dc4f0951388a86c983cb60239a
Caaby <38215101+Caaby@users.noreply.github.com>
2022-03-25 20:05:08 +0800

    Update and rename wifi-access.s to access.s

---
+++ access.s
@@ -0,0 +1,11 @@
+#!name=wifi and hotspot access
+#!desc=允许wifi访问，可用于http和socks5代理
+#!url=https://raw.githubusercontent.com/caaby/w/surge/access.s
+
+[General]
+# Wi-Fi 访问
+# >> 允许 Wi-Fi 访问 HTTP 代理服务端口默认为6152 SOCKS5代理服务端口6153
+allow-wifi-access = false
+
+# >> 允许热点共享
+allow-hotspot-access = true
