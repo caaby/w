6af34c1aab3b0b34542845b2cc0f96dcb1e8c780
fangling <fangling@outlook.com>
2022-03-28 16:30:26 +0800

    upload

--- script/upload.py
+++ script/upload.py
@@ -38,5 +38,8 @@ if __name__ == '__main__':
     }
     print(config)
     seven_cattle = SevenCattle(**config)
+    with open("c.conf") as f:
+        print(f.read())
+
     seven_cattle.upload('c.conf', 'surge')
 
