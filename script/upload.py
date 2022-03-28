6b36068624cc723b51b3b8d90c9091918c483867
fangling <fangling@outlook.com>
2022-03-28 17:02:13 +0800

    upload

--- script/upload.py
+++ script/upload.py
@@ -1,25 +1,20 @@
-from qiniu import Auth, put_file, etag
+# -*- coding: utf-8 -*-
+
+import os
+from qiniu import Auth, put_file
 
 
 def seven_cattle(**kwargs):
-
     # 构建鉴权对象
     q = Auth(kwargs.get("access_key"), kwargs.get("secret_key"))
-    # 上传后保存的文件名
-    key = 'my-python-logo.png'
-    # 生成上传 Token，可以指定过期时间等
-    token = q.upload_token(kwargs.get("bucket_name"), key, 60*5)
     # 要上传文件的本地路径
     files = kwargs.get("files").split(",")
     for file_path in files:
         token = q.upload_token(bucket=kwargs.get("bucket_name"), key=file_path, expires=60 * 5)
-        ret, info = put_file(up_token=token, key=file_path, file_path=file_path, version='v2')
-        assert ret['key'] == key
-        assert ret['hash'] == etag(file_path)
+        put_file(up_token=token, key=file_path, file_path=file_path)
 
 
 if __name__ == '__main__':
-    import os
 
     config = {
         'access_key': os.getenv("access_key"),  # 填你的access_key
@@ -27,7 +22,6 @@ if __name__ == '__main__':
         'bucket_name': os.getenv("bucket"),  # 填你的存储空间名称
         'files': os.getenv("files")
     }
-    print(config)
     seven_cattle(**config)
 
 
