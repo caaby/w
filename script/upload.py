17c89a397f974d820c71cac5f437a212130da3ad
fangling <fangling@outlook.com>
2022-03-28 15:53:10 +0800

    upload

--- script/upload.py
+++ script/upload.py
@@ -35,10 +35,7 @@ class SevenCattle:
         :return:
         """
         token = self._q.upload_token(self.bucket_name, save_file_name)
-        ret, info = put_data(token, source_file_path, save_file_name)
-        if info.status_code == 200:
-            return '/' . join([self.domain, save_file_name])
-        return None
+        put_data(token, source_file_path, save_file_name)
 
 
 if __name__ == '__main__':
@@ -48,10 +45,8 @@ if __name__ == '__main__':
         'access_key': os.getenv("access_key"),  # 填你的access_key
         'secret_key': os.getenv("secret_key"),  # 填你的secret_key
         'bucket_name': os.getenv("bucket"),  # 填你的存储空间名称
-        'domain': 'cdn.caaby.com'  # 填你的空间域名
     }
 
-    print(config)
     seven_cattle = SevenCattle(**config)
-    url = seven_cattle.upload('main.py', 'main.py')
-    print(url)
+    seven_cattle.upload('../c.conf', 'surge')
+
