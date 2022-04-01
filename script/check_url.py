bdbe6ea9390c1505eba8a588292884b54a7bb3b2
fangling <fangling@outlook.com>
2022-04-01 19:48:02 +0800

    更新文件

--- script/check_url.py
+++ script/check_url.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
-from qiniu import Auth, put_file, BucketManager
+from qiniu import Auth, BucketManager
 
 
 def seven_cattle(**kwargs):
     # 构建鉴权对象
     q = Auth(kwargs.get("access_key"), kwargs.get("secret_key"))
-    # 要上传文件的本地路径
-    files = kwargs.get("files").split(",")
-    for file_path in files:
-        token = q.upload_token(bucket=kwargs.get("bucket_name"), key=file_path, expires=60 * 5)
-        put_file(up_token=token, key=file_path, file_path=file_path)
-
     BucketManager(auth=q).fetch(url=kwargs.get("bucket_name"), bucket=kwargs.get("bucket_name"), key="list")
 
 
@@ -22,7 +16,6 @@ if __name__ == '__main__':
         'access_key': os.getenv("access_key"),  # 填你的access_key
         'secret_key': os.getenv("secret_key"),  # 填你的secret_key
         'bucket_name': os.getenv("bucket"),  # 填你的存储空间名称
-        'files': os.getenv("files")
     }
     seven_cattle(**config)
 
