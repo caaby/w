96bb6bc728fd4cf4a8c2bc5c78733c3e65a88d1f
fangling <fangling@outlook.com>
2022-04-19 13:32:25 +0800

    ok

--- panel.s
+++ panel.s
@@ -3,12 +3,12 @@
 
 [Panel]
 # << 节假日倒计时, 节日当天早上有提醒
-节假日倒计时 = script-name=节假日倒计时, update-interval=3600
+# >> 节假日倒计时 = script-name=节假日倒计时, update-interval=3600
 
 # >> 网络信息面板
 网络信息面板 = script-name=网络信息面板, title="网络状态", content="请刷新", style=info, update-interval=3600
 
 
 [Script]
-节假日倒计时 = type=generic, timeout=10, script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/Timecard.js
-网络信息面板 = type=generic, timeout=10, script-path=https://raw.githubusercontent.com/caaby/w/surge/ip.js
+# >> 节假日倒计时 = type=generic, timeout=10, script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/Timecard.js
+网络信息面板 = type=generic, timeout=600, script-path=https://raw.githubusercontent.com/caaby/w/surge/ip.js
