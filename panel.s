bba3adfa6e765755213e82fc2cfc415317f1ce3c
fangling <fangling@outlook.com>
2022-04-22 16:34:47 +0800

    panel

--- panel.s
+++ panel.s
@@ -2,13 +2,16 @@
 #!desc=自用panel收集
 
 [Panel]
-# << 节假日倒计时, 节日当天早上有提醒
-# >> 节假日倒计时 = script-name=节假日倒计时, update-interval=3600
-
+# >> Surge Pro标题,可显示启动时间,点击刷新为重载配置
+SurgePro_ReloadProfile = script-name=SurgePro_ReloadProfile,update-interval=1
 # >> 网络信息面板
 网络信息面板 = script-name=网络信息面板, title="网络状态", content="请刷新", style=info, update-interval=3600
-
+# >> 机场信息
+Sub_info = script-name=Sub_info,update-interval = 43200
 
 [Script]
-# >> 节假日倒计时 = type=generic, timeout=10, script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/Timecard.js
+SurgePro_ReloadProfile = type=generic,timeout=10,script-path= https://raw.githubusercontent.com/fishingworld/something/main/PanelScripts/surgepro_reloadprofile.js ,argument=icon=crown.fill&color=#f6c970
+
 网络信息面板 = type=generic, timeout=20, script-path=https://raw.githubusercontent.com/caaby/w/surge/ip.js
+
+Sub_info = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/sub_info_panel.js ,script-update-interval=0,argument=url=https%3A%2F%2Fsubscribe.gacloudltd.store%2Flink%2FjeSFjhzquavecr1t&reset_day=1&title=ExFlux&icon=opticaldisc&color=#5AC8FA
