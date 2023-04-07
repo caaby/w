05ec89c0d107fede1402ce571ded006b75b15570
Caaby <38215101+Caaby@users.noreply.github.com>
2023-04-07 16:49:48 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
-import sys
 import oss2
 
 yourAccessKeyId = os.environ.get('ACCESS_KEY_ID')
 yourAccessKeySecret = os.environ.get('ACCESS_KEY_SECRET')
 yourEndpoint = os.environ.get('ENDPOINT')
 bucket_name = os.environ.get('BUCKET_NAME')
+object_name = os.environ.get('FILE_NAME')
 
 # 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
 auth = oss2.Auth(yourAccessKeyId, yourAccessKeySecret)
@@ -15,7 +15,6 @@ auth = oss2.Auth(yourAccessKeyId, yourAccessKeySecret)
 # 填写Bucket名称，例如examplebucket。
 bucket = oss2.Bucket(auth, yourEndpoint, bucket_name)
 # 填写Object完整路径，例如exampledir/exampleobject.txt。Object完整路径中不能包含Bucket名称。
-object_name = sys.argv[1]
 # 填写待上传的字符串。
 # content = '{"age": 1}'
 # 设置HTTP header，例如HTTP header的名称为Content-Type，值为'application/json; charset=utf-8'。
