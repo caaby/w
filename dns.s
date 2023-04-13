e6ca7a2dd37510a25e48d54efaa648331fb8eef9
caaby <38215101+caaby@users.noreply.github.com>
2023-04-13 15:48:59 +0800

    Delete dns.s

--- dns.s
+++
@@ -1,8 +0,0 @@
-#!name=公共dns解析
-#!desc=添加公共dns解析服务 由114,腾讯,阿里云提供
-#!url=https://raw.githubusercontent.com/caaby/w/surge/dns.s
-
-[General]
-# >> 代理请求本地DNS映射
-use-local-host-item-for-proxy = true
-dns-server = 80.80.80.80, 240c::6666, 119.29.29.29,system
